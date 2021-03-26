ef1ad99e8be9ff9afc67fb287d3ad0160282eadd
Victor Lucas <victor.oliveira@itbam.org.br>
2021-03-26 10:22:59 -0400

    Initial Spring Boot

---
+++ api/src/main/java/com/finansys/api/FinansysApplication.java
@@ -0,0 +1,13 @@
+package com.finansys.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinansysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinansysApplication.class, args);
+	}
+
+}
