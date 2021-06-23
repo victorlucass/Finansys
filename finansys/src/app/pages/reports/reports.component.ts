c74eb29abed2b35091334a340d8bf6d90d63830e
victorlucas <victor.oliveira@itbam.org.br>
2021-06-23 13:57:29 -0400

    error: não está gerando os gráficos.

--- finansys/src/app/pages/reports/reports.component.ts
+++ finansys/src/app/pages/reports/reports.component.ts
@@ -16,10 +16,21 @@ export class ReportsComponent implements OnInit {
   balance: any = 0;
   expenseChartData: any;
   revenueChartData: any;
+
+  chartOptions = {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    }
+  };
+
   categories: Category[] = [];
   entries: Entry[] = [];
-  @ViewChild('month') month : ElementRef = null; //Ele se refere ao #month lá no html;
-  @ViewChild('year') year : ElementRef = null;
+  @ViewChild('month', { static: true }) month: ElementRef = null; //Ele se refere ao #month lá no html;
+  @ViewChild('year', { static: true }) year: ElementRef = null;
 
   constructor(private categoryService: CategoryService, private entryService: EntryService) { }
 
@@ -31,16 +42,77 @@ export class ReportsComponent implements OnInit {
     )
   }
 
-  generateReports(){
+  generateReports() {
     const month = this.month.nativeElement.value; //Pega o valor do #month;
     const year = this.year.nativeElement.value;
-    
-    if(!month || !year){
-      alert("Você precisa selecionar o Mês e o Ano para gerar os relatórios!")
-    } else{
-      this.entryService.getByMonthAndYear(month, year);
-    }
 
+    if (!month || !year) {
+      alert("Você precisa selecionar o Mês e o Ano para gerar os relatórios!")
+    } else {
+      this.entryService.getByMonthAndYear(month, year).subscribe(
+        this.setValues.bind(this)
+      );
+    }
+  }
+
+  private setValues(entries: Entry[]) {
+    this.entries = entries;
+    this.calculateBalance();
+    this.setChartData();
+  }
+
+  private calculateBalance() {
+    let expenseTotal = 0;
+    let revenueTotal = 0;
+
+    this.entries.filter(
+      entry => {
+        if (entry.type == 'revenue') {
+          revenueTotal += currencyFormatter.unformat(entry.amount, { code: 'BRL' });
+        } else {
+          expenseTotal += currencyFormatter.unformat(entry.amount, { code: 'BRL' });
+        }
+      });
+
+    this.expenseTotal = currencyFormatter.format(expenseTotal, { code: 'BRL' });
+    this.revenueTotal = currencyFormatter.format(revenueTotal, { code: 'BRL' });
+    this.balance = currencyFormatter.format(revenueTotal - expenseTotal, { code: 'BRL' })
+  }
+
+  private setChartData() {
+    this.revenueChartData = this.getChartData('revenue', 'Gráfico de Receita', '#9CCC65');
+    this.expenseChartData = this.getChartData('expense', 'Gráfico de Despesas', '#e03131')
+  }
+
+  private getChartData(entryType: string, title: string, color: string){
+    const chartData = [];
+    this.categories.forEach(
+      category => {
+        const filteredEntries = this.entries.filter(
+          entry => (entry.categoryId == category.id) && (entry.type == entryType)
+        );
+
+        if (filteredEntries.length > 0) {
+          const totalAmount = filteredEntries.reduce(
+            (total, entry) => total + currencyFormatter.unformat(entry.amount, {
+              code: 'BRL'
+            }, 0)
+          );
+          chartData.push({
+            categoryName: category.name,
+            totalAmount: totalAmount
+          })
+        }
+      });
+
+    return {
+      labels: chartData.map(item => item.categoryName),
+      datasets: [{
+        label: title,
+        backgroundColor: color,
+        data: chartData.map(item => item.totalAmount)
+      }]
+    }
   }
 
 }
